06e62169a68b6f9aae65435835dcd047a794406c
ko tainen <tainenko@gmail.com>
2019-03-25 11:29:30 +0800

    update openweather api setting

--- app.py
+++ app.py
@@ -7,4 +7,8 @@ app.config['DEBUG']=True
 
 @app.route('/')
 def index():
+    url='http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=8625b46b1df662e9223ab2fa4771e56f'
+    city='Las Vegas'
+    r=requests.get(url.format(city)).json()
+    print(r)
     return render_template('weather.html')
