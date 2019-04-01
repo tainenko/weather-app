4b8dc79406c2864a49006cde56ad9de742156769
ko tainen <tainenko@gmail.com>
2019-04-01 19:00:01 +0800

    增加weather.db以及完善dashboard的city查詢功能

--- app.py
+++ app.py
@@ -1,14 +1,39 @@
 import requests
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.config['DEBUG']=True
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///weather.db'
 
-@app.route('/')
+db=SQLAlchemy(app)
+class City(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(50),nullable=False)
+
+
+@app.route('/',methods=['GET','POST'])
 def index():
+    if request.method =='POST':
+        new_city = request.form.get('city')
+        if new_city and not City.query.filter_by(name=new_city).first():
+            new_city_obj = City(name=new_city)
+            db.session.add(new_city_obj)
+            db.session.commit()
+
+    cities=City.query.all()
     url='http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=8625b46b1df662e9223ab2fa4771e56f'
-    city='Las Vegas'
-    r=requests.get(url.format(city)).json()
-    print(r)
-    return render_template('weather.html')
+    weather_data=[]
+
+    for city in cities:
+        r=requests.get(url.format(city.name)).json()
+
+        weather={
+            'city':city.name,
+            'temperature':r['main']['temp'],
+            'description':r['weather'][0]['description'],
+            'icon':r['weather'][0]['icon']
+        }
+
+        weather_data.append(weather)
+    return render_template('weather.html',weather_data=weather_data)
